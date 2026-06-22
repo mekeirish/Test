bfa2bed1f00cab20144e652ef6ede9f6f79ecac4
mekeirish <mmm1747lb@gmail.com>
2026-06-22 23:02:23 +0200

    Hubcode: fast UP update via clipboard (sw.js)

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
 self.addEventListener('push', function(event) {
-    const data = event.data.text();
-    const options = {
-        body: data,
-        icon: 'icon.png'
-    };
+    const data = event.data ? event.data.text() : 'Nouvelle notification';
     event.waitUntil(
-        self.registration.showNotification('Nouvelle Alerte', options)
+        self.registration.showNotification('Alerte Système', {
+            body: data,
+            icon: './icon.png' // Assurez-vous d'avoir une image icon.png ou supprimez cette ligne
+        })
     );
 });
