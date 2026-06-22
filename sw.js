a7a9ea6912398e877788f5efe21f178f6d6ce3c9
mekeirish <mmm1747lb@gmail.com>
2026-06-22 22:51:23 +0200

    Hubcode: fast UP update via clipboard (sw.js)

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-const cacheName='Test-v1'; self.addEventListener('install',e=>{e.waitUntil(caches.open(cacheName).then(c=>c.addAll(['./index.html','./manifest.json','./icon.png'])));}); self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+self.addEventListener('push', function(event) {
+    const data = event.data.text();
+    const options = {
+        body: data,
+        icon: 'icon.png'
+    };
+    event.waitUntil(
+        self.registration.showNotification('Nouvelle Alerte', options)
+    );
+});
