51380abcb748eb9971feb948eb7370b90020a1e5
mekeirish <mmm1747lb@gmail.com>
2026-07-28 23:20:07 +0200

    Mastercode Sync

--- sw.js
+++ sw.js
@@ -1,9 +1,1 @@
-self.addEventListener('push', event => {
-    const data = event.data ? event.data.text() : 'Nouvelle alerte';
-    event.waitUntil(
-        self.registration.showNotification('Mon Alerte PWA', {
-            body: data,
-            icon: './icon.png'
-        })
-    );
-});
+const cacheName='Test-v1'; self.addEventListener('install',e=>{e.waitUntil(caches.open(cacheName).then(c=>c.addAll(['./index.html','./manifest.json','./icon.png'])));}); self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
